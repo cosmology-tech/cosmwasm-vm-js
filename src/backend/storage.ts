014197a5f72f91ec6b968d027fe9ea6134e355b5
Bun <bunchhieng@gmail.com>
2022-08-26 00:40:49 -0400

    Fixed tests

--- src/backend/storage.ts
+++ src/backend/storage.ts
@@ -7,8 +7,6 @@ export interface IStorage {
   set(key: Uint8Array, value: Uint8Array): void;
 
   remove(key: Uint8Array): void;
-
-  clear(): void;
 }
 
 export interface Iter {
@@ -32,84 +30,6 @@ export interface IIterStorage {
 
   scan(start: Uint8Array | null, end: Uint8Array | null, order: Order): number; // Uint32
   next(iterator_id: number /* Uint32 */): Record | null;
-
-  clear(): void;
-}
-
-export class BasicKVIterStorage implements IIterStorage {
-  constructor(
-    public data: Map<Uint8Array, Uint8Array> = new Map(),
-    public iterators: Map<number, Iter> = new Map()
-  ) {}
-
-  all(iterator_id: number): Array<Record> {
-    const out: Array<Record> = [];
-    let condition = true;
-    while (condition) {
-      const record = this.next(iterator_id);
-      if (record === null) {
-        condition = false;
-      } else {
-        out.push(record);
-      }
-    }
-    return out;
-  }
-
-  get(key: Uint8Array): Uint8Array | null {
-    const value = this.data.get(key);
-    if (value === undefined) {
-      return null;
-    }
-    return value;
-  }
-
-  // Get next element of iterator with ID `iterator_id`.
-  // Creates a region containing both key and value and returns its address.
-  // Ownership of the result region is transferred to the contract.
-  // The KV region uses the format value || key || keylen, where keylen is a fixed size big endian u32 value.
-  // An empty key (i.e. KV region ends with \0\0\0\0) means no more element, no matter what the value is.
-  next(iterator_id: number): Record | null {
-    const iter = this.iterators.get(iterator_id);
-    if (iter === undefined) {
-      throw new Error(`Iterator ${iterator_id} not found.`);
-    }
-    const record = iter.data[iter.position];
-    iter.position += 1;
-    return record;
-  }
-
-  remove(key: Uint8Array): void {
-    if (key.length > MAX_LENGTH_DB_KEY) {
-      throw new Error(
-        `Key length ${key.length} exceeds maximum length ${MAX_LENGTH_DB_KEY}.`
-      );
-    }
-    this.data.delete(key);
-  }
-
-  // Creates an iterator that will go from start to end.
-  // If start_ptr == 0, the start is unbounded.
-  // If end_ptr == 0, the end is unbounded.
-  // Order is defined in cosmwasm_std::Order and may be 1 (ascending) or 2 (descending). All other values result in an error.
-  // Ownership of both start and end pointer is not transferred to the host.
-  // Returns an iterator ID.
-  scan(start: Uint8Array | null, end: Uint8Array | null, order: Order): number {
-    throw new Error('Not implemented');
-  }
-
-  set(key: Uint8Array, value: Uint8Array): void {
-    this.data.set(key, value);
-  }
-
-  clear(): void {
-    this.data.clear();
-  }
-}
-
-export class Record {
-  public key: Uint8Array = Uint8Array.from([]);
-  public value: Uint8Array = Uint8Array.from([]);
 }
 
 export class BasicKVStorage implements IStorage {
@@ -139,10 +59,48 @@ export class BasicKVStorage implements IStorage {
     }
     this.dict[toBase64(key)] = undefined;
   }
+}
 
-  clear() {
-    Object.keys(this.dict).forEach((key) => {
-      delete this.dict[key];
-    });
+export class BasicKVIterStorage extends BasicKVStorage implements IIterStorage {
+  constructor(public iterators: Map<number, Iter> = new Map()) {
+    super();
   }
+
+  all(iterator_id: number): Array<Record> {
+    const out: Array<Record> = [];
+    let condition = true;
+    while (condition) {
+      const record = this.next(iterator_id);
+      if (record === null) {
+        condition = false;
+      } else {
+        out.push(record);
+      }
+    }
+    return out;
+  }
+
+  // Get next element of iterator with ID `iterator_id`.
+  // Creates a region containing both key and value and returns its address.
+  // Ownership of the result region is transferred to the contract.
+  // The KV region uses the format value || key || keylen, where keylen is a fixed size big endian u32 value.
+  // An empty key (i.e. KV region ends with \0\0\0\0) means no more element, no matter what the value is.
+  next(iterator_id: number): Record | null {
+    const iter = this.iterators.get(iterator_id);
+    if (iter === undefined) {
+      throw new Error(`Iterator ${iterator_id} not found.`);
+    }
+    const record = iter.data[iter.position];
+    iter.position += 1;
+    return record;
+  }
+
+  scan(start: Uint8Array | null, end: Uint8Array | null, order: Order): number {
+    throw new Error('Not implemented');
+  }
+}
+
+export class Record {
+  public key: Uint8Array = Uint8Array.from([]);
+  public value: Uint8Array = Uint8Array.from([]);
 }
