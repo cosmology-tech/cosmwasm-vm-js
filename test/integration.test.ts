694d8273ed0833bb520b128f339af7e396ced35d
Luke Wilson <luke.wilson@intelligenthack.com>
2022-09-06 14:32:30 +0100

    Remove unused params and refactor tests a bit

--- test/integration.test.ts
+++ test/integration.test.ts
@@ -39,13 +39,13 @@ describe('integration', () => {
     await vm.build(wasm_byte_code);
 
     // Act
-    const region = vm.instantiate(mock_env, mock_info, {
+    const instantiateResponse = vm.instantiate(mock_env, mock_info, {
       verifier: 'terra1kzsrgcktshvqe9p089lqlkadscqwkezy79t8y9',
       beneficiary: 'terra1zdpgj8am5nqqvht927k3etljyl6a52kwqup0je'
     });
 
     // Assert
-    expect(region.json).toEqual({
+    expect(instantiateResponse.json).toEqual({
       ok: {
         attributes: [
           { key: 'Let the', value: 'hacking begin' },
@@ -55,43 +55,112 @@ describe('integration', () => {
         messages: [],
       },
     });
+    expectVerifierToBe('terra1kzsrgcktshvqe9p089lqlkadscqwkezy79t8y9');
   });
 
   it('instantiate_and_query', async () => {
     // Arrange
-    await vm.build(wasm_byte_code);
-    vm.instantiate(mock_env, mock_info, {
-      verifier: 'terra1kzsrgcktshvqe9p089lqlkadscqwkezy79t8y9',
-      beneficiary: 'terra1zdpgj8am5nqqvht927k3etljyl6a52kwqup0je'
-    });
+    await instantiate();
 
     // Act
-    const response = vm.query(mock_env, { verifier: {} });
-    const verifier = parseResponse(response);
+    const queryResponse = vm.query(mock_env, { verifier: {} });
 
     // Assert
-    expect(verifier).toEqual({ verifier: 'terra1kzsrgcktshvqe9p089lqlkadscqwkezy79t8y9' });
+    expectResponseToBeOk(queryResponse);
+    expect(parseBase64Response(queryResponse)).toEqual({ verifier: 'terra1kzsrgcktshvqe9p089lqlkadscqwkezy79t8y9' });
   });
 
-  it('migrate_verifier', async () => {});
-  it('sudo_can_steal_tokens', async () => {});
+  it('migrate_verifier', async () => {
+    // Arrange
+    await instantiate();
+
+    // Act
+    let response = vm.migrate(mock_env, { verifier: 'terra1h8ljdmae7lx05kjj79c9ekscwsyjd3yr8wyvdn' });
+
+    // Assert
+    expectResponseToBeOk(response);
+    expect((response.json as { ok: { messages: any[] }}).ok.messages.length).toBe(0);
+    expectVerifierToBe('terra1h8ljdmae7lx05kjj79c9ekscwsyjd3yr8wyvdn');
+  });
+
+  it.skip('sudo_can_steal_tokens', async () => {
+    throw new Error('Not implemented');
+  });
+
   it('querier_callbacks_work', async () => {});
+
   it('fails_on_bad_init', async () => {});
+
   it('execute_release_works', async () => {});
+
   it('execute_release_fails_for_wrong_sender', async () => {});
+
   it('execute_argon2', async () => {});
+
   it('execute_cpu_loop', async () => {});
+
   it('execute_storage_loop', async () => {});
+
   it('execute_memory_loop', async () => {});
+
   it('execute_allocate_large_memory', async () => {});
+
   it('execute_panic', async () => {});
+
   it('execute_user_errors_in_api_calls', async () => {});
-  it('passes_io_tests', async () => {});
+
+  it.skip('passes_io_tests', async () => {
+    throw new Error('Not implemented');
+  });
 });
 
 // Helpers
 
-function parseResponse(region: Region): any {
+function expectResponseToBeOk(region: Region) {
+  try {
+    expect((region.json as { ok: string }).ok).toBeDefined();
+  } catch (_) {
+    throw new Error(`Expected response to be ok; instead got: ${JSON.stringify(region.json)}`);
+  }
+}
+
+async function instantiate() {
+  await vm.build(wasm_byte_code);
+  vm.instantiate(mock_env, mock_info, {
+    verifier: 'terra1kzsrgcktshvqe9p089lqlkadscqwkezy79t8y9',
+    beneficiary: 'terra1zdpgj8am5nqqvht927k3etljyl6a52kwqup0je'
+  });
+}
+
+function expectVerifierToBe(addr: string) {
+  const queryResponse = vm.query(mock_env, { verifier: {} });
+  const verifier = parseBase64Response(queryResponse);
+  expect(verifier).toEqual({ verifier: addr });
+}
+
+function parseBase64Response(region: Region): any {
   const data = (region.json as { ok: string }).ok;
-  return JSON.parse(fromAscii(fromBase64(data)));
+  if (!data) {
+    throw new Error(`Response indicates an error state: ${JSON.stringify(region.json)}`)
+  }
+
+  let bytes: Uint8Array;
+  try {
+    bytes = fromBase64(data);
+  } catch (_) {
+    throw new Error(`Data value is not base64-encoded: ${JSON.stringify(data)}`)
+  }
+
+  let str: string;
+  try {
+    str = fromAscii(bytes);
+  } catch (_) {
+    throw new Error(`Data value is not ASCII encoded: ${JSON.stringify(bytes)}`)
+  }
+
+  try {
+    return JSON.parse(str);
+  } catch (_) {
+    throw new Error(`Data value is not valid JSON: ${str}`)
+  }
 }
